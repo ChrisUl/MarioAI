ac9918acf3c648b54879c8ba8a3814a247a7537f
Dorian Weidler <dorian.weidler@hs-kl.de>
2017-04-06 13:07:32 +0200

    Levels modified and simplified to just LEVEL_X

--- MarioAI-Simulation/src/main/java/de/novatec/marioai/simulation/LevelConfig.java
+++ MarioAI-Simulation/src/main/java/de/novatec/marioai/simulation/LevelConfig.java
@@ -5,47 +5,23 @@ import ch.idsia.benchmark.mario.options.FastOpts;
 
 public enum LevelConfig {
 	
-	/**
-	 * Level for workshop 1 exercise 1
-	 */
-	HSKL_LEVEL_0(FastOpts.VIS_ON_2X + FastOpts.LEVEL_01_FLAT),
-	
-	/**
-	 * Level for workshop 1 exercise 2
-	 */
-	HSKL_LEVEL_1(FastOpts.VIS_ON_2X + FastOpts.LEVEL_01_FLAT + FastOpts.L_ENEMY(Enemy.GOOMBA, Enemy.GREEN_KOOPA)),
-	
-	/**
-	 * Level for workshop 1 exercise 3
-	 */
-	HSKL_LEVEL_2(FastOpts.VIS_ON_2X + FastOpts.LEVEL_01_FLAT + FastOpts.L_ENEMY(Enemy.GOOMBA, Enemy.GREEN_KOOPA, Enemy.SPIKY)),
-	
-	/**
-	 * Level for workshop 1 exercise 4
-	 */
-	HSKL_LEVEL_3(FastOpts.VIS_ON_2X + FastOpts.L_RANDOM_SEED(41) + FastOpts.L_GAPS_ON + FastOpts.L_PLATFORMS_ON + FastOpts.L_COINS_OFF),
-	
-	/**
-	 * Level for workshop 1 exercise 5
-	 */
-	HSKL_LEVEL_4(FastOpts.VIS_ON_2X + FastOpts.L_GAPS_ON + FastOpts.L_PLATFORMS_ON + FastOpts.L_TUBES_ON + FastOpts.L_ENEMY(Enemy.GOOMBA, Enemy.GREEN_KOOPA, Enemy.SPIKY)),
+	LEVEL_1(FastOpts.VIS_ON_2X + FastOpts.LEVEL_01_FLAT),
 
-	LEVEL_1(FastOpts.VIS_ON_2X + FastOpts.L_FLAT_OFF + FastOpts.L_BLOCKS_OFF + FastOpts.L_CANNONS_OFF + FastOpts.L_COINS_OFF + FastOpts.L_DEAD_ENDS_OFF + FastOpts.L_GAPS_OFF
-			+ FastOpts.L_HIDDEN_BLOCKS_OFF + FastOpts.L_PLATFORMS_OFF + FastOpts.L_LADDERS_OFF + FastOpts.L_TUBES_OFF + FastOpts.L_DIFFICULTY(0)),
+	LEVEL_2(FastOpts.VIS_ON_2X + FastOpts.LEVEL_01_FLAT + FastOpts.L_ENEMY(Enemy.GOOMBA, Enemy.GREEN_KOOPA)),
 
-	LEVEL_2(FastOpts.VIS_ON_2X + FastOpts.L_FLAT_OFF + FastOpts.L_BLOCKS_OFF + FastOpts.L_CANNONS_OFF + FastOpts.L_COINS_OFF + FastOpts.L_DEAD_ENDS_OFF + FastOpts.L_GAPS_OFF
-			+ FastOpts.L_HIDDEN_BLOCKS_OFF + FastOpts.L_PLATFORMS_OFF + FastOpts.L_LADDERS_OFF + FastOpts.L_TUBES_OFF + FastOpts.L_DIFFICULTY(0) + FastOpts.L_ENEMY(Enemy.GOOMBA) ),
-
-	LEVEL_3(FastOpts.VIS_ON_2X + FastOpts.L_FLAT_OFF + FastOpts.L_BLOCKS_OFF + FastOpts.L_CANNONS_OFF + FastOpts.L_COINS_OFF + FastOpts.L_DEAD_ENDS_OFF + FastOpts.L_GAPS_OFF
-			+ FastOpts.L_HIDDEN_BLOCKS_OFF + FastOpts.L_PLATFORMS_OFF + FastOpts.L_LADDERS_OFF + FastOpts.L_TUBES_OFF + FastOpts.L_DIFFICULTY(0) + FastOpts.L_ENEMY(Enemy.GREEN_KOOPA)),
+	LEVEL_3(FastOpts.VIS_ON_2X + FastOpts.L_RANDOM_SEED(41) + FastOpts.L_GAPS_ON + FastOpts.L_PLATFORMS_ON + FastOpts.L_COINS_OFF),
 
 
-	LEVEL_4(FastOpts.VIS_ON_2X + FastOpts.L_FLAT_OFF + FastOpts.L_BLOCKS_OFF + FastOpts.L_CANNONS_OFF + FastOpts.L_COINS_OFF + FastOpts.L_DEAD_ENDS_OFF + FastOpts.L_GAPS_OFF
-			+ FastOpts.L_HIDDEN_BLOCKS_OFF + FastOpts.L_PLATFORMS_OFF + FastOpts.L_LADDERS_OFF + FastOpts.L_TUBES_OFF + FastOpts.L_DIFFICULTY(0) + FastOpts.L_ENEMY(Enemy.SPIKY)),
+	LEVEL_4(FastOpts.VIS_ON_2X + FastOpts.L_GAPS_ON + FastOpts.L_PLATFORMS_ON + FastOpts.L_TUBES_ON + FastOpts.L_ENEMY(Enemy.GOOMBA, Enemy.GREEN_KOOPA, Enemy.SPIKY)),
 
-
-	LEVEL_5(FastOpts.VIS_ON_2X + FastOpts.L_FLAT_OFF + FastOpts.L_BLOCKS_OFF + FastOpts.L_CANNONS_OFF + FastOpts.L_COINS_OFF + FastOpts.L_DEAD_ENDS_OFF + FastOpts.L_GAPS_OFF
-			+ FastOpts.L_HIDDEN_BLOCKS_OFF + FastOpts.L_PLATFORMS_OFF + FastOpts.L_LADDERS_OFF + FastOpts.L_TUBES_OFF + FastOpts.L_DIFFICULTY(0) + FastOpts.L_ENEMY(Enemy.SPIKY,Enemy.GOOMBA));
+	LEVEL_5(FastOpts.VIS_ON_2X
+			+ FastOpts.L_RANDOM_SEED(2708)
+			+ FastOpts.L_DEAD_ENDS_ON
+			+ FastOpts.L_GAPS_ON
+			+ FastOpts.L_PLATFORMS_ON
+			+ FastOpts.L_TUBES_ON
+			+ FastOpts.L_ENEMY(Enemy.GOOMBA, Enemy.GREEN_KOOPA, Enemy.SPIKY)
+			+ FastOpts.L_COINS_ON);
 
 	private String options;
 
